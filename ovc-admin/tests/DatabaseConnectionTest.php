e885b8c646dedf47121b5e99c624b1ab3834b353
Bartosz Pazdur <b.pazdur@yahoo.com>
2023-04-02 09:19:22 +0200

    Created database connection test, fixed php issues and fixed workflow

---
+++ ovc-admin/tests/DatabaseConnectionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class DatabaseConnectionTest extends KernelTestCase
+{
+    protected function setUp(): void
+    {
+        self::bootKernel();
+    }
+
+    #[Test]
+    public function databaseConnectionIsSet(): void
+    {        
+        $container = self::$kernel->getContainer();
+        $doctrine = $container->get('doctrine')->getConnection();
+
+        $doctrine->connect();
+        $this->assertTrue($doctrine->isConnected(), 'Application does not have connection to database');
+    }
+}
